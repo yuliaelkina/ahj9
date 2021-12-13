4b6dcad2d7ab1ecc3c54e11fc9e352549dc3dce1
yulia elkina <todg@mail.ru>
2021-12-13 12:06:25 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ahj7
+[![Build status](https://ci.appveyor.com/api/projects/status/60crmi2oy65j17d2?svg=true)](https://ci.appveyor.com/project/yuliaelkina/ahj7)
---
+++ webpack.common.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  target: 'web',
+  entry: path.resolve(__dirname, '/src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    assetModuleFilename: 'img/[name][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+      {
+        test: /\.png$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.txt$/,
+        type: 'asset/source',
+        generator: {
+          filename: '[hash][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
